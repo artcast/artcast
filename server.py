50601b1c04644754ee754ad5698d3feab75767dd
Timothy M. Shead <tim@shead-custom-design.com>
2012-11-12 21:38:39 -0700

    Add a simple HTML UI that exercises long-polling functionality.

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 import functools
 import json
+import os
 import socket
 import threading
 
 import tornado.ioloop
 import tornado.web
 
-class number_handler(tornado.web.RequestHandler):
+class handler(tornado.web.RequestHandler):
+  """Add some useful functionality common to all handlers."""
+  def accept(self, *mime_types):
+    for mime_type in mime_types:
+      for element in self.request.headers["accept"].split(","):
+        if mime_type == element.split(";")[0]:
+          return mime_type
+
+class number_handler(handler):
   """Handles each request for a number."""
   callbacks = set()
   @staticmethod
@@ -18,6 +27,10 @@ class number_handler(tornado.web.RequestHandler):
   @tornado.web.asynchronous
   def get(self, key):
     """Called when a client requests a number."""
+    if self.accept("application/json", "text/html") == "text/html":
+      self.render("number.html", key=key)
+      return
+
     self.key = key
     number_handler.callbacks.add(self.get_result)
 
@@ -29,9 +42,16 @@ class number_handler(tornado.web.RequestHandler):
     self.write(str(data))
     self.finish()
 
-application = tornado.web.Application([
+application = tornado.web.Application(
+  [
   (r"/numbers/(.*)", number_handler)
-  ])
+  ],
+  debug = True,
+  static_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "content"),
+  static_url_prefix = "/content/"
+  )
+
+print os.path.join(os.path.abspath(os.path.dirname(__file__)), "content")
 
 def udp_source():
   """Listens for new numbers."""
