810a96775ef7ef3d9661734263d53ef3eb496c17
Timothy M. Shead <tim@shead-custom-design.com>
2012-11-14 03:37:04 +0000

    Update the api / ui to our new name: artcast!

--- server.py
+++ server.py
@@ -15,58 +15,56 @@ class handler(tornado.web.RequestHandler):
         if mime_type == element.split(";")[0]:
           return mime_type
 
-class number_handler(handler):
-  """Handles each request for a number."""
+class artcast_handler(handler):
+  """Handles each request for an artcast."""
   callbacks = set()
   @staticmethod
   def message(key, data):
-    """Called whenever a new number has been received."""
-    for callback in number_handler.callbacks.copy():
+    """Called whenever a new artcast value has been received."""
+    for callback in artcast_handler.callbacks.copy():
       callback(key, data)
 
   @tornado.web.asynchronous
   def get(self, key):
-    """Called when a client requests a number."""
+    """Called when a client requests an artcast."""
     if self.accept("application/json", "text/html") == "text/html":
-      self.render("number.html", key=key)
+      self.render("artcast.html", key=key)
       return
 
     self.key = key
-    number_handler.callbacks.add(self.get_result)
+    artcast_handler.callbacks.add(self.get_result)
 
   def get_result(self, key, data):
-    """Called when a new number has been received."""
+    """Called when a new artcast value has been received."""
     if self.key != key:
       return
-    number_handler.callbacks.remove(self.get_result)
+    artcast_handler.callbacks.remove(self.get_result)
     self.set_header("Content-Type", "application/json")
     self.write(json.dumps(data))
     self.finish()
 
 application = tornado.web.Application(
   [
-  (r"/numbers/(.*)", number_handler)
+  (r"/artcasts/(.*)", artcast_handler)
   ],
   debug = True,
   static_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "content"),
   static_url_prefix = "/content/"
   )
 
-print os.path.join(os.path.abspath(os.path.dirname(__file__)), "content")
-
 def udp_source():
-  """Listens for new numbers."""
+  """Listens for new artcast values."""
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   s.bind(("", 51423))
   while True:
     key, data = json.loads(s.recv(4096))
-    tornado.ioloop.IOLoop.instance().add_callback(functools.partial(number_handler.message, key, data))
+    tornado.ioloop.IOLoop.instance().add_callback(functools.partial(artcast_handler.message, key, data))
 
 if __name__ == "__main__":
   udp_thread = threading.Thread(target=udp_source)
   udp_thread.daemon = True
   udp_thread.start()
 
-  application.listen(8888)
+  application.listen(80)
   tornado.ioloop.IOLoop.instance().start()
 
