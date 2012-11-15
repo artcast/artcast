37ca72ed40d36e15eaa9d57848b159c94d19d700
Timothy M. Shead <tim@shead-custom-design.com>
2012-11-14 19:28:18 -0700

    Switch to multicast UDP so each source can communicate with multiple servers.

--- packages/artcast/__init__.py
+++ packages/artcast/__init__.py
@@ -1,12 +1,12 @@
 import json
 import optparse
 import socket
+import sys
 import threading
 import time
 
-host = "localhost"
-port = 51423
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+options = None
 threads = []
 
 def add(target):
@@ -14,16 +14,24 @@ def add(target):
   threads[-1].daemon = True
 
 def send(key, value):
-  s.sendto(json.dumps((key, value)), (host, port))
+  if options.verbose:
+    sys.stderr.write("send %s : %s\n" % (key, value))
+  sock.sendto(json.dumps((key, value)), (options.group, options.port))
 
 def run():
+  global options
   parser = optparse.OptionParser()
-  parser.add_option("--host", default="localhost", help="Server host.  Default: %default")
-  parser.add_option("--port", type="int", default=51423, help="Server input port.  Default: %default")
+  parser.add_option("--group", default="224.1.1.1", help="Multicast group for sending source messages.  Default: %default")
+  parser.add_option("--port", type="int", default=5007, help="Multicast port for sending source messages.  Default: %default")
+  parser.add_option("--ttl", type="int", default=1, help="Multicast TTL.  Default: %default")
+  parser.add_option("--verbose", default=False, action="store_true",  help="Enable debugging output.")
   options, arguments = parser.parse_args()
 
-  host = options.host
-  port = options.port
+  if options.verbose:
+    for key in sorted(options.__dict__.keys()):
+      sys.stderr.write("%s : %s\n" % (key, options.__dict__[key]))
+
+  sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, options.ttl)
 
   for thread in threads:
     thread.start()
--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@ import functools
 import json
 import os
 import socket
+import struct
+import sys
 import threading
 
 import tornado.httpserver
@@ -44,23 +46,35 @@ class artcast_handler(handler):
     self.write(json.dumps(data))
     self.finish()
 
-def udp_source(port):
+def udp_source(group, port, verbose):
   """Listens for new artcast values."""
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  s.bind(("", port))
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+  sock.bind(("", port))
+  mreq = struct.pack("4sl", socket.inet_aton(group), socket.INADDR_ANY)
+  sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
   while True:
-    key, data = json.loads(s.recv(4096))
+    key, data = json.loads(sock.recv(4096))
+    if options.verbose:
+      sys.stderr.write("recv %s : %s\n" % (key, data))
     tornado.ioloop.IOLoop.instance().add_callback(functools.partial(artcast_handler.message, key, data))
 
 if __name__ == "__main__":
   import optparse
   parser = optparse.OptionParser()
   parser.add_option("--client-port", type="int", default=8888, help="Client request input port.  Default: %default")
-  parser.add_option("--source-port", type="int", default=51423, help="Source data port.  Default: %default")
+  parser.add_option("--source-group", default="224.1.1.1", help="Multicast group for receiving source messages.  Default: %default")
+  parser.add_option("--source-port", type="int", default=5007, help="Multicast port for receiving source messages.  Default: %default")
   parser.add_option("--uid", type="int", default=None, help="Drop privileges to uid.  Default: %default")
+  parser.add_option("--verbose", default=False, action="store_true",  help="Enable debugging output.")
   options, arguments = parser.parse_args()
 
-  udp_thread = threading.Thread(target=udp_source, kwargs={"port" : options.source_port})
+  if options.verbose:
+    for key in sorted(options.__dict__.keys()):
+      sys.stderr.write("%s : %s\n" % (key, options.__dict__[key]))
+
+  udp_thread = threading.Thread(target=udp_source, kwargs={"group" : options.source_group, "port" : options.source_port, "verbose" : options.verbose})
   udp_thread.daemon = True
   udp_thread.start()
 
